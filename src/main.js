7fe35ca5a79c60b2432f3007486d35939bb74f68
Rabel Obispo <rabelobispo@hotmail.com>
2020-01-25 14:22:37 -0500

    interceptors

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue';
 import VueResource from 'vue-resource';
 
 Vue.use(VueResource);
+
 Vue.http.options.root = 'https://vue-http-7e0ae.firebaseio.com/data.json';
+
+Vue.http.interceptors.push((r, n) => {
+  console.log(r);
+  n(nr => console.log(nr));
+});
 
 new Vue({
   el: '#app',
